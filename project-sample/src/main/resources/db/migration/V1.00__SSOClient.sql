23a1da639ec9360675037dbc14b4fe1b2f2b8185
lilj <hongyullj@sohu.com>
2021-07-09 15:35:15 +0800

    增加日志处理版块

--- project-sample/src/main/resources/db/migration/V1.00__SSOClient.sql
+++ project-sample/src/main/resources/db/migration/V1.00__SSOClient.sql
@@ -68,19 +68,43 @@ DELIMITER ;
 DROP TABLE IF EXISTS `oauth_client_details`;
 CREATE TABLE `oauth_client_details`
 (
-    `client_id`               varchar(256)   NOT NULL COMMENT '客户端ID',
-    `resource_ids`            varchar(256)   NULL DEFAULT NULL COMMENT '客户端能够访问的资源ID，暂时没用',
-    `client_secret`           varchar(256)   NULL DEFAULT NULL COMMENT '客户端密钥',
-    `scope`                   varchar(256)   NULL DEFAULT NULL COMMENT '授权范围',
-    `authorized_grant_types`  varchar(256)   NULL DEFAULT NULL COMMENT '授权方式',
-    `web_server_redirect_uri` varchar(256)   NULL DEFAULT NULL COMMENT '授权导向地址',
-    `authorities`             varchar(256)   NULL DEFAULT NULL COMMENT '',
-    `access_token_validity`   int        NULL DEFAULT NULl COMMENT 'token访问时间',
-    `refresh_token_validity`  int       NULL DEFAULT NULL COMMENT 'token刷新时间',
-    `additional_information`  varchar(4096)  NULL DEFAULT NULL COMMENT '附加信息',
-    `autoapprove`             varchar(256)   NULL DEFAULT NULL COMMENT '是否自动授权',
+    `client_id`               varchar(256)  NOT NULL COMMENT '客户端ID',
+    `resource_ids`            varchar(256)  NULL DEFAULT NULL COMMENT '客户端能够访问的资源ID，暂时没用',
+    `client_secret`           varchar(256)  NULL DEFAULT NULL COMMENT '客户端密钥',
+    `scope`                   varchar(256)  NULL DEFAULT NULL COMMENT '授权范围',
+    `authorized_grant_types`  varchar(256)  NULL DEFAULT NULL COMMENT '授权方式',
+    `web_server_redirect_uri` varchar(256)  NULL DEFAULT NULL COMMENT '授权导向地址',
+    `authorities`             varchar(256)  NULL DEFAULT NULL COMMENT '',
+    `access_token_validity`   int           NULL DEFAULT NULl COMMENT 'token访问时间',
+    `refresh_token_validity`  int           NULL DEFAULT NULL COMMENT 'token刷新时间',
+    `additional_information`  varchar(4096) NULL DEFAULT NULL COMMENT '附加信息',
+    `autoapprove`             varchar(256)  NULL DEFAULT NULL COMMENT '是否自动授权',
     PRIMARY KEY (`client_id`) USING BTREE
-) ;
+);
+
+-- 系统日志记录表
+DROP TABLE IF EXISTS `sys_log`;
+CREATE TABLE `sys_log`
+(
+    `logID`              varchar(40)   NOT NULL COMMENT '日志id',
+    `logTime`            datetime(0)   NULL DEFAULT CURRENT_TIMESTAMP(0) COMMENT '时间',
+    `userid`             varchar(45)   NULL DEFAULT NULL COMMENT '登录用户ID',
+    `username`           varchar(45)   NULL DEFAULT NULL COMMENT '用户姓名',
+    `requestIP`          varchar(45)   NULL DEFAULT NULL COMMENT '请求IP',
+    `requestUrl`         varchar(45)   NULL DEFAULT NULL COMMENT '请求url',
+    `requestClassMethod` varchar(100)  NULL DEFAULT NULL COMMENT '请求类名方法',
+    `executeTime`        int           NULL DEFAULT NULL COMMENT '执行时长',
+    `modelName`          varchar(45)   NULL DEFAULT NULL COMMENT '模块名称',
+    `opeType`            varchar(45)   NULL DEFAULT NULL COMMENT '操作类型 1 修改 2 删除 3 数据展示 4 登录 5 退出 6其它',
+    `description`        varchar(200)  NULL DEFAULT NULL COMMENT '描述信息',
+    `logType`            int           NULL DEFAULT NULL COMMENT '类型，扩展字段',
+    `reqParam`           varchar(2000) NULL DEFAULT NULL COMMENT '请求参数，以json格式存储',
+    `errorCode`          varchar(20)   NULL DEFAULT NULL COMMENT '错误代码',
+    `errDescription`     varchar(4000) NULL DEFAULT NULL COMMENT '错误信息',
+    PRIMARY KEY (`logID`) USING BTREE
+) COMMENT = '系统日志表';
+
+SET FOREIGN_KEY_CHECKS = 1;
 
 -- ----------------------------
 -- 示例数据
